dfed87fdd07ecbd6a50ffd0f410b9709bb94e832
amy2525 <yam532004@gmail.com>
2026-01-13 17:38:34 +0700

    feat: add remove_task function

--- simple_task_manager/core.py
+++ simple_task_manager/core.py
@@ -1,5 +1,8 @@
 def add_task(task):
     return f"Task '{task}' added"
 
+def remove_task(task: str) -> str:
+    return f"Task removed: {task}"
+
 def list_tasks():
     return ["Learn Git", "Practice Release"]
